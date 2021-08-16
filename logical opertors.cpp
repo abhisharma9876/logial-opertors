e877647acab9fa23c493658df6e9f75edb5c5c1c
abhisharma9876 <86251863+abhisharma9876@users.noreply.github.com>
2021-08-16 08:50:11 +0530

    Add files via upload

---
+++ logical opertors.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int hour;
+	cout<<"enter the hours";
+	cin>>hour;
+	if(hour>=9&&hour<=18)
+	{
+		cout<<"working";
+		
+	}
+	else
+	{
+		cout<<"leisure";
+	}
+	return 0;
+}
